9b358fcefb695c0552a9de3830d266c4846530a3
Eduardo Torozze <92118764+EduardoTorozze@users.noreply.github.com>
2024-02-29 10:37:08 -0300

    Add files via upload

---
+++ Scripts/index.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+const mobileLinks = document.querySelector('.mobile__links')
+
+button.addEventListener('click', function(){
+    mobileNavbar.classList.toggle('active');
+});
+
+mobileLinks.addEventListener('click', function(){
+    mobileNavbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function(){
+    if(this.window.scrollY > 0) return navbar.classList.add('active');
+    return navbar.classList.remove('active');
+});
